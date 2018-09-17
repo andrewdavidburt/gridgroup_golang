f366eab6fbb560b2246a36b1e17043c4c44145cb
Andrew David Burt <andrewdavid@Andrews-MacBook-Pro.local>
2018-09-16 17:20:08 -0700

    runs, is running the loop one extra time after successfully traversing groups, adding to count and loop executions

--- gridgroup.go
+++ gridgroup.go
@@ -34,36 +34,54 @@ func DefineGrid() [][]bool {
 func FloodFillAlgo(grid [][]bool, width int, height int, x int, y int) [][]bool {
 	if grid[x][y] == true {
 		grid[x][y] = false
-
+//fmt.Println("Pos:",grid[x][y])
+//fmt.Println("NW")
 	if (x > 0) &&		(y > 0) 	{FloodFillAlgo(grid, width, height, x-1, y-1)} //NW
+//fmt.Println("N")
 	if 			(y > 0) 	{FloodFillAlgo(grid, width, height, x, y-1)} //N
+//fmt.Println("NE")
 	if (x < width-1) &&	(y > 0) 	{FloodFillAlgo(grid, width, height, x+1, y-1)} //NE
 
+//fmt.Println("W")
 	if (x > 0)				{FloodFillAlgo(grid, width, height, x-1, y)} //W
+//fmt.Println("E")
 	if (x < width-1)			{FloodFillAlgo(grid, width, height, x+1, y)} //E
 
+//fmt.Println("SW")
 	if (x > 0) && 		(y < height-1) 	{FloodFillAlgo(grid, width, height, x-1, y+1)} //SW
+//fmt.Println("S")
 	if 			(y < height-1)	{FloodFillAlgo(grid, width, height, x, y+1)} //S
+//fmt.Println("SE")
 	if (x < width-1) &&	(y < height-1)	{FloodFillAlgo(grid, width, height, x+1, y+1)} //SE
 
         } else {
+//fmt.Println("returngrid")
                 return grid
         }
 //this seems unnecessary but not sure how to avoid it
+//fmt.Println("weirdreturngrid")
 	return grid
 }
 
 func DefineGroups(grid [][]bool) (int, [][]bool) {
 	var count int = 0
-	var width = len(grid)
-	var height = len(grid[0])
+	var width = len(grid)-1
+//fmt.Println("width:", width)
+	var height = len(grid[0])-1
+//fmt.Println("height:", height)
 //	var newGrid = [][]bool
 	
+fmt.Println("Start DefineGroups")
 	for x := 0; x <= width; x++ {
+//fmt.Println("inside x", x)
 		for y := 0; y <= height; y++ {
+//fmt.Println("inside y", y)
 			if grid[x][y] == true {
+//fmt.Println("grid_define_groups:", grid)
 				grid = FloodFillAlgo(grid, width, height, x, y)
 				count = count + 1
+fmt.Println("New count:", count)
+fmt.Println("Grid:", grid)
 			}
 		}
 	}
@@ -73,6 +91,7 @@ func DefineGroups(grid [][]bool) (int, [][]bool) {
 
 func main() {
 	var grid [][]bool = DefineGrid()
+fmt.Println("Grid:", grid)
 	var groupct int
 	var groupdef [][]bool
 	//change this and in definegroups output to [][]int to define groups?
