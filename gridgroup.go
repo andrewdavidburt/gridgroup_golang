8fb510dcbb2accaea30ee598ecc3453e3e1751b4
Andrew David Burt <andrewdavid@Andrews-MacBook-Pro.local>
2018-09-17 23:58:33 -0700

    fixing formatting, etc

--- gridgroup.go
+++ gridgroup.go
@@ -36,41 +36,41 @@ type Coord struct {
 //	Grouping []Group
 //}
 
-func FloodFillAlgo(grid [][]bool, width int, height int, x int, y int, count int, group_3 [][]int) ([][]bool, [][]int) {
+func FloodFillAlgo(grid [][]bool, width int, height int, x int, y int, count int, group [][]int) ([][]bool, [][]int) {
 
 	if grid[x][y] == true {
 		grid[x][y] = false
 		xy_temp := Coord{X: x, Y: y}
 		fmt.Println("GROUP:", count, "ELEMENT:", xy_temp)
-		group_3[count] = append(group_3[count], x, y)
+		group[count] = append(group[count], x, y)
 		
 	if (x > 0) &&		(y > 0) 	{	//NW
-						FloodFillAlgo(grid, width, height, x-1, y-1, count, group_3)}
+						FloodFillAlgo(grid, width, height, x-1, y-1, count, group)}
 	if 			(y > 0) 	{	//N
-						FloodFillAlgo(grid, width, height, x, y-1, count, group_3)}
+						FloodFillAlgo(grid, width, height, x, y-1, count, group)}
 	if (x < width-1) &&	(y > 0) 	{	//NE)
-						FloodFillAlgo(grid, width, height, x+1, y-1, count, group_3)}
+						FloodFillAlgo(grid, width, height, x+1, y-1, count, group)}
 
 	if (x > 0)				{	//W
-						FloodFillAlgo(grid, width, height, x-1, y, count, group_3)}
+						FloodFillAlgo(grid, width, height, x-1, y, count, group)}
 	if (x < width-1)			{	//E
-						FloodFillAlgo(grid, width, height, x+1, y, count, group_3)}
+						FloodFillAlgo(grid, width, height, x+1, y, count, group)}
 
 	if (x > 0) && 		(y < height-1) 	{	//SW
-						FloodFillAlgo(grid, width, height, x-1, y+1, count, group_3)}
+						FloodFillAlgo(grid, width, height, x-1, y+1, count, group)}
 	if 			(y < height-1)	{	//S
-						FloodFillAlgo(grid, width, height, x, y+1, count, group_3)}
+						FloodFillAlgo(grid, width, height, x, y+1, count, group)}
 	if (x < width-1) &&	(y < height-1)	{	//SE
-						FloodFillAlgo(grid, width, height, x+1, y+1, count, group_3)}
+						FloodFillAlgo(grid, width, height, x+1, y+1, count, group)}
 
         } else {
-                return grid, group_3
+                return grid, group
         }
 
-	return grid, group_3
+	return grid, group
 }
 
-func DefineGroups(grid [][]bool, group_3 [][]int) (int, [][]int) {
+func DefineGroups(grid [][]bool, group [][]int) (int, [][]int) {
 	var count int = 0
 	var width = 4		//len(grid)
 	var height = 4		//len(grid[0])
@@ -78,20 +78,20 @@ func DefineGroups(grid [][]bool, group_3 [][]int) (int, [][]int) {
 	for x := 0; x < width; x++ {
 		for y := 0; y < height; y++ {
 			if grid[x][y] == true {
-				grid, group_3 = FloodFillAlgo(grid, width, height, x, y, count, group_3)
+				grid, group = FloodFillAlgo(grid, width, height, x, y, count, group)
 				count = count + 1
 			}
 		}
 	}
 
-	return count, group_3
+	return count, group
 }
 
 func main() {
 	var grid [][]bool = DefineGrid()
-	group_3 := make([][]int, 5)
+	group := make([][]int, 5)
 	var groupct int
-	groupct, group_3 = DefineGroups(grid, group_3)
-	fmt.Println("output group_3", group_3)
+	groupct, group = DefineGroups(grid, group)
+	fmt.Println("Output Separated Groups in pairs of x y coordinates:", group)
 	fmt.Println("Group Count:", groupct)
 }
