3c96b69516fa05f312ae41da52876205253f2621
Andrew David Burt <andrewdavid@Andrews-MacBook-Pro.local>
2018-09-17 21:48:17 -0700

    added count tests to definegroups and floodfillalgo

--- gridgroup_test.go
+++ gridgroup_test.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"testing"
-	"fmt"
+//	"fmt"
 )
 
 // the grid definitions are stored in slices of y- (vertical) stripes
@@ -91,44 +91,10 @@ func Test_FloodFillAlgo(t *testing.T) {
 }
 
 func Test_DefineGroups(t *testing.T) {
-fmt.Println("")
+	var grid [][]bool = DefineGrid()
+	var groupct int
+	groupct = DefineGroups(grid)
+	if groupct != 2 {
+		t.Error("Expected group count 2, got:", groupct)
+	}
 }
-
-//func Test_fib(t *testing.T) {
-//	var f []int
-//	var n int=10
-//	var l int = 100
-//	var c = make([]int, 10)
-//	c[0] = 1
-//	c[1] = 2
-//	c[2] = 3
-//	c[3] = 5
-//	c[4] = 8
-//	c[5] = 13
-//	c[6] = 21
-//	c[7] = 34
-//	c[8] = 55
-//	c[9] = 89
-//	f = fib(n, l)
-//	if len(c) != len(f) {
-//		t.Error("Expected length:", len(c), "got length:", len(f))
-//	}
-//	for i, v:= range f {
-//		if v != c[i] {
-//			fmt.Println(v, c[i])
-//			t.Error("Expected:", c[i], "got:", v)
-//		}
-//	}
-//}
-//		
-//func Test_addevens(t *testing.T) {
-//	var f []int
-//	var n int=10
-//	var l int = 100
-//	f = fib(n, l)
-//	var ae int
-//	ae = addevens(f)
-//	if (ae != 44) {
-//		t.Error("Expected 44, got:", ae)
-//	}
-//}
