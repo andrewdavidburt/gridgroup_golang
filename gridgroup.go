dc8d4dbbb2cfa18854297d61f8f443c280552388
Andrew David Burt <andrewdavid@Andrews-MacBook-Pro.local>
2018-09-17 22:32:42 -0700

    trying to get a better data export working

--- gridgroup.go
+++ gridgroup.go
@@ -32,15 +32,16 @@ type Group struct {
 	Coords []Coord
 }
 
-type Collection struct {
-//	Number int
-	Grouping []Group
-}
+//type Collection struct {
+////	Number int
+//	Grouping []Group
+//}
 
-// eliminate this global variable
+// eliminate global variables later
 //var group_temp []int
 var group_new_temp Group
 //var collection_temp Collection
+var allgroups []Group = 
 
 func FloodFillAlgo(grid [][]bool, width int, height int, x int, y int, count int, group Group) ([][]bool, Group) {
 
@@ -53,9 +54,17 @@ func FloodFillAlgo(grid [][]bool, width int, height int, x int, y int, count int
 		//fmt.Println("grnew_temp", group_new_temp)
 		//group_temp = append(group_temp, count, x, y)
 		group.Coords = append(group.Coords, xy_temp)
+
+		fmt.Println("allgroups", allgroups)
+		//allgroups[count].Coords = xy_temp
+		allgroups[count].Coords = append(allgroups[count].Coords, xy_temp)
+
 //		fmt.Println("appending", xy_temp, "to group.Coords:", group.Coords, "of group", group)
 //		fmt.Println("==============")
+		
 //remove count in flood fill algo later
+//if using globals, remove group passing in flood fill algo later
+
 	if (x > 0) &&		(y > 0) 	{	//NW
 						FloodFillAlgo(grid, width, height, x-1, y-1, count, group)}
 	if 			(y > 0) 	{	//N
@@ -87,20 +96,20 @@ func DefineGroups(grid [][]bool) (int) {
 	var count int = 0
 	var width = 4		//len(grid)
 	var height = 4		//len(grid[0])
-	fmt.Println("len", len(grid))
+	//fmt.Println("len", len(grid))
 	var groupvar []Group
 	var group Group
-	
+
 	for x := 0; x < width; x++ {
 		for y := 0; y < height; y++ {
 			if grid[x][y] == true {
 				grid, group = FloodFillAlgo(grid, width, height, x, y, count, group)
 				count = count + 1
-				fmt.Println("New count:", count)
-				fmt.Println("group_new_temp:", group_new_temp)
-				fmt.Println("group", group)
+				//fmt.Println("New count:", count)
+				//fmt.Println("group_new_temp:", group_new_temp)
+				//fmt.Println("group", group)
 				groupvar = append(groupvar, group)
-				fmt.Println("groupvar", groupvar)
+				//fmt.Println("groupvar", groupvar)
 			}
 		}
 	}
@@ -110,11 +119,12 @@ func DefineGroups(grid [][]bool) (int) {
 
 func main() {
 	var grid [][]bool = DefineGrid()
-	fmt.Println("Grid:", grid)
+//	allgroups := make([]Group, 1)
+	//fmt.Println("Grid:", grid)
 	var groupct int
 	groupct = DefineGroups(grid)
-	fmt.Println("FINAL RESULTS")
+	//fmt.Println("FINAL RESULTS")
 	fmt.Println("Group Count:", groupct)
 	//fmt.Println(group_temp)
-	fmt.Println(group_new_temp)
+	//fmt.Println(group_new_temp)
 }
